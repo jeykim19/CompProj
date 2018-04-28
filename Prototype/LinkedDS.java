6edb784a3d80297d2cb92c777d42d8e7f6d5f5af
Xinzhu-Fang <xf15@hampshire.edu>
2018-04-28 00:41:27 -0400

    Asteroids are not stored in Linked List

--- Prototype/LinkedDS.java
+++ Prototype/LinkedDS.java
@@ -3,7 +3,8 @@
 public class LinkedDS<E> implements GenericOrderedCollection<E>{
 
   // your code here
-  private Node<E> end;
+  public Node<E> end;
+  public int length = 0;
   public  LinkedDS(){
     end = null;
   }
@@ -11,6 +12,7 @@ public class LinkedDS<E> implements GenericOrderedCollection<E>{
     Node toAdd = new Node(toAppend);
     toAdd.previous = end;
     end = toAdd;
+    length += 1;
   }
   public E peek(){
     if (end!= null){
@@ -27,6 +29,7 @@ public class LinkedDS<E> implements GenericOrderedCollection<E>{
     if (end!= null){
       toReturn = end.num;
       end = end.previous;
+      length -= 1;
     }
     return toReturn;
   }
@@ -44,13 +47,14 @@ public class LinkedDS<E> implements GenericOrderedCollection<E>{
 
   }
   public int length(){
-    int intsLength = 0;
-    Node n = end;
-    while(n != null){
-      intsLength ++;
-      n = n.previous;
-    }
-    return intsLength;
+    return length;
+    // int intsLength = 0;
+    // Node n = end;
+    // while(n != null){
+    //   intsLength ++;
+    //   n = n.previous;
+    // }
+    // return intsLength;
 
 
   }
@@ -67,6 +71,8 @@ public class LinkedDS<E> implements GenericOrderedCollection<E>{
 
       }
       removed.previous = removed.previous.previous;
+      length -= 1;
+
 
     } else {
       throw new java.lang.Error("No node indexed");
