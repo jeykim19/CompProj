c2077bc7dbc6469d7a40eb37e9321deff531bf8d
jgoravens678 <jgoravens678@gmail.com>
2018-05-02 03:04:14 -0400

    Add files via upload

--- LinkedDS.java
+++ LinkedDS.java
@@ -1,151 +1,153 @@
-//package oc;
+public class LinkedDS<E> implements GenericOrderedCollection<E> {
 
-public class LinkedDS<E> implements GenericOrderedCollection<E>{
+    Node<E> start;
+    Node<E> end;
+    int myCurrentLength;
 
-  // your code here
-  public Node<E> end;
-  public  LinkedDS(){
-    end = null;
-  }
-  public void append(E toAppend){
-    Node toAdd = new Node(toAppend);
-    toAdd.previous = end;
-    end = toAdd;
-  }
-  public E peek(){
-    if (end!= null){
-        return end.client;
 
-      } else{
-        return null;
-      }
+    public LinkedDS(){
+        this.start = null;
+        this.end = null;
+        this.myCurrentLength = 0;
     }
+    public void append(E toAppend){
+        Node<E> tmp = new Node<E>(toAppend);
+        if(myCurrentLength == 0){
+            start = tmp;
+            end = tmp;
+            myCurrentLength++;
+            return;
+        }
+        end.next = tmp;
+        tmp.previous = end;
+        end = tmp;
 
-
-  public E pop(){
-    E toReturn = null;
-    if (end!= null){
-      toReturn = end.client;
-      end = end.previous;
+        myCurrentLength++;
     }
-    return toReturn;
-  }
-
-  public String toString(){
-    String allInts = "";
-    Node n = end;
-    while (n != null){
-      allInts = n.client + " " + allInts;
-      n = n.previous;
-
-
+    public E peek(){
+        if(end == null){
+            return null;
+        }
+        return end.element;
     }
-    return allInts;
-
-  }
-  public int length(){
-    int intsLength = 0;
-    Node n = end;
-    while(n != null){
-      intsLength ++;
-      n = n.previous;
-    }
-    return intsLength;
-
-
-  }
-
-  public void remove(int index){
-
-    if ((index<length())&&(index>-1)){
-      int iIndex = length()-1;
-      if (iIndex == index) {
+    public E pop(){
+        if(end == null){
+            return null;
+        }
+        E element = peek();
         end = end.previous;
+        end.next = null;
+        myCurrentLength--;
+        return element;
+    }
+    public void remove(E element) {
 
-      }else{
-        Node afterRemoved = end;
-        while(iIndex != index + 1){
-        afterRemoved = afterRemoved.previous;
-        iIndex -= 1;
-
-
+        if (myCurrentLength == 0) {
+            //System.out.println("The list is empty");
+            return;
         }
-        afterRemoved.previous = afterRemoved.previous.previous;
 
-      }
-      }
-       else {
-      throw new java.lang.Error("No node indexed");
-
-    }
-  }
-
-  public E get(int index){
-
-    if ((index<length())&&(index>-1)){
-      int iIndex = length()-1;
-      if (iIndex == index) {
-E target = end.client;
-      }else{
-        Node afterTarget = end;
-        while(iIndex != index + 1){
-        afterTarget = afterTarget.previous;
-        iIndex -= 1;
-
-
+        Node<E> n = end;
+        while (n != null) {
+            if (n.element == element) {
+                eradicate(n);
+            }
+            n = n.previous;
         }
-        afterTarget.previous = afterTarget.previous.previous;
-
-      }
-      }
-       else {
-      throw new java.lang.Error("No node indexed");
-
-    }
-  }
-
-
-
-
-  public void remove(E element){
-    if (length()==0){
-      System.out.println("This list is empty.");
+        myCurrentLength--;
     }
 
-    int counter=0;
-    Node <E> n=end; //Node<E> n= end;
-    Node <E> p=end; //Node<E> p=end;
-    while (n!=null){
-      counter=counter+1;
+    public void remove(int index){
+        //The node at the ith index is the node that is 'length - i-1' nodes in on the tree
+        //We want: this.child --> this.parent and this.parent --> this.child
+        if(myCurrentLength == 0){
+            //System.out.println("The list is empty");
+            return;
+        }
+        if(index >= myCurrentLength || index < 0){
+            //System.out.println("This is not a valid index");
+            return;
+        }
+        if(index == myCurrentLength - 1){
+            end.previous.next = null;
+        }
 
-      if (n.element == element){  //replace value with element in actual one
-	  if ((counter-1)>0){
-	      while ((count-1)>0){
-		  p=p.previous; //replace nextSpace with previous
-		  counter=counter-1;
-	      }
-	      p=p.previous.previous;
-	  }
-	  else if ((counter-1)==0){
-	      end=end.previous;
-	  }
-      }
-      n=n.previous;
+        int counter = 0;
+
+        Node<E> n = end;
+        counter = 0;
+
+        while(counter < myCurrentLength - index - 1){
+            counter++;
+            n = n.previous;
+        }
+        //This removes it
+        //n.parent = n.parent.parent;
+        eradicate(n);
+
+        myCurrentLength--;
     }
-  }
+
+    private void eradicate(Node<E> n){
+        if(n.previous != null && n.next != null) {
+            n.previous.next = n.next;
+            n.next.previous = n.previous;
+        }
+        else if (n.previous == null && n.next != null) {
+            n.next.previous = null;
+        }
+        else if (n.next == null && n.previous != null) {
+            n.previous.next = null;
+        }
+        else{
+            start = null;
+            end = null;
+        }
+    }
+
+    public String toString(){
+        String sequence = "";
+        Node<E> n = end;
+        while(n != null){
+            sequence = n.element + " " + sequence;
+            n = n.previous;
+        }
+        return sequence;
+    }
 
 
+    public int length(){
+        return myCurrentLength;
+    }
+    public boolean isEmpty(){
+        if(myCurrentLength == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public Node<E> get(E element){
+        Node<E> n  = end;
+        while(n != null){
+            if(n.element == element){
+                return n;
+            }
+            n = n.previous;
+        }
+        return null;
+    }
+}
+
+class Node<E>{
+    E element;
+    Node<E> previous;
+    Node<E> next;
+
+    public Node(E element){
+        this.element = element;
+        //this.previous = previous;
+        //this.next = next;
+    }
+}
 
 
-
-
- }
-
-
- class Node<E>{
- E client;
- Node previous;
- public Node(E client){
-   this.client = client;
- }
- }
